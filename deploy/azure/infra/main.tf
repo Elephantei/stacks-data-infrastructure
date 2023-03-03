cd8fc1adee2c84afccbc19cab4d243f25210856c
jakub.slon <jakubslon@gmail.com>
2023-03-03 16:10:16 +0000

    fix/amend-module-call

--- deploy/azure/infra/main.tf
+++ deploy/azure/infra/main.tf
@@ -22,8 +22,7 @@
 
 # module "adls-de" {
 #   source = "git::https://github.com/amido/stacks-terraform//azurerm/modules/azurerm-data-platform?ref=vXXXXXX"
-#   resource_namer = module.default_label.id
-#   default_storage_account_name = var.default_storage_account_name
-#   region                       = var.region
-#   resource_group_name          = azurerm_resource_group.default.name
+#   resource_namer          = module.default_label.id
+#   resource_group_name     = azurerm_resource_group.default.name
+#   resource_group_location = azurerm_resource_group.default.location
 # }
